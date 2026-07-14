7dc83dbed6be556636a7dfbba2d0b268f30cd69c
longvite8 <longvite8@gmail.com>
2026-07-14 14:32:29 +0700

    Fix typo in hyperparameter key for n_estimators in run_hpo_optimization function

--- business/hpo.py
+++ business/hpo.py
@@ -13,7 +13,7 @@ def run_hpo_optimization(X_train, y_train, X_valid, y_valid, n_trials, random_st
         model_params = {
             "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.1, log=True),
             "num_leaves": trial.suggest_int("num_leaves", 15, 100),
-            "n_estimators": trial.suggest_int("num_estimators", 100, 1000),
+            "n_estimators": trial.suggest_int("n_estimators", 100, 1000),
             "random_state": random_state,
         }
 

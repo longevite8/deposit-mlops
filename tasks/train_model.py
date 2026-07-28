0ea3b12032707c9d56f96cacab32b6631d5eb4a5
duongnh2 <duongnh2@vega.com.vn>
2026-07-28 08:21:28 +0700

    mark train task without template refresh

--- tasks/train_model.py
+++ tasks/train_model.py
@@ -42,7 +42,7 @@ from config import (
 from helpers import wait_for_artifact
 
 
-CACHE_EXPERIMENT_TRACE = "scenario_2_train_logic_marker"
+CACHE_EXPERIMENT_TRACE = "scenario_3_no_template_reregister_marker"
 
 
 def truthy(value) -> bool:

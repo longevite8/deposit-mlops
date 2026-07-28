022dca3818c3973ab69d7e68ef4bf335328ad2a0
duongnh2 <duongnh2@vega.com.vn>
2026-07-28 11:03:53 +0700

    mark train cache scenario 3 retry

--- tasks/train_model.py
+++ tasks/train_model.py
@@ -42,7 +42,7 @@ from config import (
 from helpers import wait_for_artifact
 
 
-CACHE_EXPERIMENT_TRACE = "scenario_2_retry_train_only_template_refresh"
+CACHE_EXPERIMENT_TRACE = "scenario_3_retry_no_template_refresh"
 
 
 def truthy(value) -> bool:
